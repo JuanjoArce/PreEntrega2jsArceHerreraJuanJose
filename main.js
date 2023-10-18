35633122882c9f8fc54e3078b2198ae1452bbc07
JuanjoArce <juanjosearceherrera@gmail.com>
2023-10-18 10:51:25 -0300

    implementacion de js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function Persona(nombre, apellido, ingreso, egreso){
+function Persona(nombre, apellido, ingreso, egreso) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.ingreso = ingreso;
@@ -6,45 +6,64 @@ function Persona(nombre, apellido, ingreso, egreso){
     this.saldo = ingreso - egreso;
 }
 
-// Crear un array para almacenar multiples personas
-let personas = []
+const personas = loadFromLocalStorage() || [];
 
-// Solicitar al usuario que ingrese datos para crear una nueva Persona
-let nombre = prompt("Ingrese su nombre:");
-let apellido = prompt("Ingrese su Apellido");
-let ingreso = parseFloat(prompt("Indique su ingresos:"));
-let egreso = parseFloat(prompt("Ingrese su egresos"));
+const agregarPersonaButton = document.getElementById("agregarPersona");
+const personaForm = document.getElementById("personaForm");
+const nombreInput = document.getElementById("nombre");
+const apellidoInput = document.getElementById("apellido");
+const ingresoInput = document.getElementById("ingreso");
+const egresoInput = document.getElementById("egreso");
+const guardarPersonaButton = document.getElementById("guardarPersona");
+const personasTableBody = document.getElementById("personasTableBody");
 
-//Crear una instancia de persona con los datos proporcionados por el usuario
-let nuevaPersona = new Persona(nombre, apellido, ingreso, egreso);
+agregarPersonaButton.addEventListener("click", () => {
+    personaForm.style.display = "block";
+});
 
-// Agregar la nueva Persona al Array de personsa 
-personas.push(nuevaPersona);
+guardarPersonaButton.addEventListener("click", () => {
+    const nombre = nombreInput.value;
+    const apellido = apellidoInput.value;
+    const ingreso = parseFloat(ingresoInput.value);
+    const egreso = parseFloat(egresoInput.value);
 
-//Agregar a otra persona
-let agregarOtra = confirm("Desea agregar otra persona?");
-while(agregarOtra){
-    nombre = prompt("Ingrese el nombre:");
-    apellido = prompt("Ingrese el apellido:");
-    ingreso = parseFloat(prompt("Ingrese su ingresos:"));
-    egreso = parseFloat(prompt("Ingrese sus gastos"));
+    if (nombre && apellido && !isNaN(ingreso) && !isNaN(egreso)) {
+        const nuevaPersona = new Persona(nombre, apellido, ingreso, egreso);
+        personas.push(nuevaPersona);
+        saveToLocalStorage(personas);
+        updatePersonasTable();
+        resetForm();
+        personaForm.style.display = "none";
+    }
+});
 
-    nuevaPersona = new Persona(nombre, apellido, ingreso, egreso);
-    personas.push(nuevaPersona);
-
-    agregarOtra = confirm("Desea agregar otra persona?")
+function updatePersonasTable() {
+    personasTableBody.innerHTML = "";
+    for (let i = 0; i < personas.length; i++) {
+        const persona = personas[i];
+        const row = personasTableBody.insertRow();
+        row.insertCell(0).textContent = persona.nombre;
+        row.insertCell(1).textContent = persona.apellido;
+        row.insertCell(2).textContent = persona.ingreso;
+        row.insertCell(3).textContent = persona.egreso;
+        row.insertCell(4).textContent = persona.saldo;
+    }
 }
 
-// Mostrar los datos
-let mensaje = "Datos de todas las personas:\n";
-for (let i = 0; i < personas.length; i++) {
-    let persona = personas[i];
-    mensaje += "Persona " + (i + 1) + ":\n" +
-            "Nombre: " + persona.nombre + "\n" +
-            "Apellido: " + persona.apellido + "\n" +
-            "Ingreso: $" + persona.ingreso + "\n" +
-            "Egreso: $" + persona.egreso + "\n" +
-            "Saldo: $" + persona.saldo + "\n\n";
+function resetForm() {
+    nombreInput.value = "";
+    apellidoInput.value = "";
+    ingresoInput.value = "";
+    egresoInput.value = "";
 }
 
-alert(mensaje);
+function saveToLocalStorage(data) {
+    localStorage.setItem("personasData", JSON.stringify(data));
+}
+
+function loadFromLocalStorage() {
+    const data = localStorage.getItem("personasData");
+    return data ? JSON.parse(data) : null;
+}
+
+updatePersonasTable();
